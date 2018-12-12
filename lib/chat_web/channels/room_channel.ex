c7479b6e4afcbb2f438fe9f8577ee58ff32c20dd
Jesse <jesse@gigalixir.com>
2018-12-11 19:31:27 -0800

    remove some of the database stuff and remove the after join stuff. following https://phoenixframework.org/blog/the-road-to-2-million-websocket-connections

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -3,7 +3,6 @@ defmodule ChatWeb.RoomChannel do
 
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
-      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -19,26 +18,12 @@ defmodule ChatWeb.RoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (chat_room:lobby).
   def handle_in("shout", payload, socket) do
-    {:ok, msg} =
-      Chat.Message.changeset(%Chat.Message{}, payload)
-      |> Chat.Repo.insert()
-
-    broadcast(socket, "shout", Map.put_new(payload, :id, msg.id))
+    broadcast(socket, "shout", Map.put_new(payload, :id, 0))
     {:noreply, socket}
   end
 
   # example see: https://git.io/vNsYD
   def handle_info(:after_join, socket) do
-    # get messages 10 - 1000
-    Chat.Message.get_messages()
-    |> Enum.each(fn msg ->
-      push(socket, "shout", %{
-        name: msg.name,
-        message: msg.message,
-        id: msg.id
-      })
-    end)
-
     # :noreply
     {:noreply, socket}
   end
