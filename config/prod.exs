0bf48e4d785c5620b6f0052f9360cdcbc32a9478
Jesse <jesse@gigalixir.com>
2018-12-11 19:37:16 -0800

    workaround error.

--- config/prod.exs
+++ config/prod.exs
@@ -28,6 +28,7 @@ config :logger, level: :info
 config :chat, Chat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
+  database: "",
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
